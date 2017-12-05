57aa01ad78308a5fb46c41478dc790fc4009dd81
Simon Stemplinger <simon@stem.ps>
2017-12-05 08:53:06 +0100

    use anonymous class

--- spec/loggery/controller/logging_context_spec.rb
+++ spec/loggery/controller/logging_context_spec.rb
@@ -6,29 +6,31 @@ module Loggery
   module Controller
     describe LoggingContext do
       describe "when included" do
-        class self::Spy
-          class << self
-            cattr_accessor :before_action_set
+        let(:spy_class) do
+          Class.new do
+            class << self
+              attr_accessor :before_action_set
 
-            def before_action(action_name)
-              self.before_action_set = true if action_name == :loggery_set_metadata
+              def before_action(action_name)
+                self.before_action_set = true if action_name == :loggery_set_metadata
+              end
             end
-          end
 
-          include LoggingContext
-          def loggery_log_context
-            { foo: :bar }
+            include LoggingContext
+            def loggery_log_context
+              { foo: :bar }
+            end
           end
         end
 
         before { Loggery::Metadata::Store.init_store }
 
         it "registers a before_action" do
-          expect(self.class::Spy.before_action_set).to be_truthy
+          expect(spy_class.before_action_set).to be_truthy
         end
 
         it "accepts custom log context" do
-          self.class::Spy.new.loggery_set_metadata
+          spy_class.new.loggery_set_metadata
           expect(Loggery::Metadata::Store.store).to include(foo: :bar)
         end
       end
